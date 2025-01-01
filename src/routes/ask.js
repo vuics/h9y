75bcc35e897317b97c503e88b4ce8bf3f282abaa
Artem Arakcheev <artarakcheev@gmail.com>
2025-01-01 11:47:46 +0300

    Plan code for executing agents in notebooks in comments

--- src/routes/ask.js
+++ src/routes/ask.js
@@ -4,6 +4,8 @@ import { checkAuth, checkAPIAuth } from '../middleware/check-auth.js'
 import Dialog from '../models/dialog.js'
 import { Verbose } from '../services.js'
 import conf from '../conf.js'
+// import util from 'util'
+// import { exec } from 'child_process'
 
 const verbose = Verbose('sd:routes/ask'); verbose('')
 const router = Router()
@@ -21,6 +23,29 @@ const ask = async (req, res, next) => {
     const dialog = new Dialog({ userId: req.user._id, prompt, reply })
     res.json({ result: 'ok', reply })
     await dialog.save()
+
+    // Code for execution
+    // verbose('prompt:', prompt)
+    // if (prompt[0]==='!') {
+    //   const exec = util.promisify(exec);
+    //   const command = prompt.substring(1);
+    //   verbose('ask command:',command)
+    //   async function execute() {
+    //     const { stdout, stderr } = await exec(command);
+    //     console.log('execute stdout:', stdout);
+    //     console.log('execute stderr:', stderr);
+    //   }
+    //   await execute();
+    //   res.json({ result: 'ok', reply: stdout || stderr })
+    // } else {
+    //   const response = await axios.get(`${conf.snake.url}/ask`, {
+    //     params: { prompt }
+    //   })
+    //   const reply = response.data
+    //   const dialog = new Dialog({ userId: req.user._id, prompt, reply })
+    //   res.json({ result: 'ok', reply })
+    //   await dialog.save()
+    }
   } catch (err) {
     res.json({ result: 'error', message: err.toString()})
   }
