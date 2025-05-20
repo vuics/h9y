91d2a8fdc0d09e53a371401c152f9a76faf2cda3
Artem Arakcheev <artarakcheev@gmail.com>
2025-05-20 15:32:26 +0300

    Generate secure XMPP password

--- src/routes/xmpp.js
+++ src/routes/xmpp.js
@@ -1,6 +1,8 @@
+import { randomBytes } from 'crypto';
 import { Router } from 'express'
 import axios from 'axios'
 import { v4 as uuidv4 } from 'uuid'
+import generator from 'generate-password'
 import { checkAuth, checkAPIAuth } from '../middleware/check-auth.js'
 import { Verbose } from '../services.js'
 import conf from '../conf.js'
@@ -16,7 +18,8 @@ const credentials = async (req, res, next) => {
     if (req.user?.xmpp?.user && req.user?.xmpp?.password) {
       user = req.user.xmpp.user;
       password = req.user.xmpp.password;
-      verbose('Using existing XMPP credentials> user:', user, ', password:', password);
+      // verbose('Using existing XMPP credentials> user:', user, ', password:', password);
+      verbose('Using existing XMPP credentials for user:', user);
     } else {
       // FIXME: The problem of uniqueness. What if this user is not unique?
       //        It registers the same user with a new password that is not accessible
@@ -24,8 +27,16 @@ const credentials = async (req, res, next) => {
       //        Possible solutions: check if that user already exists in Prosody or in Mongo (User.find())
       //        Or allow users to select a unique nickname.
       user = req.user.firstName + req.user.lastName
-      password = uuidv4()
-      verbose('Register a new XMPP user with credentials> user:', user, ', password:', password)
+
+      password = generator.generate({
+        length: 16,
+        numbers: true,
+        symbols: true,
+        uppercase: true,
+        strict: true,
+      })
+      verbose('Register a new XMPP user:', user)
+      // verbose('Register a new XMPP user with credentials> user:', user, ', password:', password)
 
       try {
         const response = await axios({
