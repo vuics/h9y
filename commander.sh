0b6f4c7b20702cb63c9207f30f4d14789de89ea8
Artem Arakcheev <artarakcheev@gmail.com>
2025-06-03 21:32:45 +0300

    Update commander to register agents on subdomains

--- commander.sh
+++ commander.sh
@@ -5,5 +5,7 @@
 
 # shell2http -host 0.0.0.0 -port 8387 -form /register 'prosodyctl register $v_user $v_host $v_password'
 
-shell2http -export-all-vars -host 0.0.0.0 -port 8387 -form -cgi /register 'out=$(prosodyctl register $v_user $v_host $v_password 2>&1) ; [ $? -eq 0 ] && echo "Status: 200\n\nRegistration successfull" || echo "Status: 500\n\nRegistration failed"; echo "Command: prosodyctl register $v_user $v_host $v_password 2>&1\n"; echo "Command output:\n$out";'
+# shell2http -export-all-vars -host 0.0.0.0 -port 8387 -form -cgi /register 'out=$(prosodyctl register $v_user $v_host $v_password 2>&1) ; [ $? -eq 0 ] && echo "Status: 200\n\nRegistration successfull" || echo "Status: 500\n\nRegistration failed"; echo "Command: prosodyctl register $v_user $v_host $v_password 2>&1\n"; echo "Command output:\n$out";'
+
+shell2http -export-all-vars -host 0.0.0.0 -port 8387 -form -cgi /register 'out=$(prosodyctl shell "host:activate('\''$v_host'\'')" ; prosodyctl shell "user:create('\''$v_user@$v_host'\'', '\''$v_password'\'', '\'''\'')" 2>&1) ; [ $? -eq 0 ] && echo "Status: 200\n\nRegistration successfull" || echo "Status: 500\n\nRegistration failed"; echo "Command: prosodyctl register $v_user $v_host $v_password 2>&1\n"; echo "Command output:\n$out";'
 
