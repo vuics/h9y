affede62ad3a102e5729ab7cba9bf1efdb125b89
Artem Arakcheev <artarakcheev@gmail.com>
2025-07-07 19:48:39 +0300

    Update user limits on logout

--- src/routes/logout.js
+++ src/routes/logout.js
@@ -1,12 +1,19 @@
 import { Router } from 'express'
+
 import { checkAuth } from '../middleware/check-auth.js'
-import { Verbose } from '../services.js'
+import { Verbose, error } from '../services.js'
+import { updateUserLimits } from './subscriptions.js'
 
 const verbose = Verbose('sd:routes/logout'); verbose('')
 
 const router = Router()
 
-router.get('/', checkAuth, (req, res, next) => {
+router.get('/', checkAuth, async (req, res, next) => {
+  try {
+    await updateUserLimits({ user: req.user })
+  } catch (err) {
+    error('Error updating user limits on logout:', err)
+  }
   const userEmail = req.user.email
   req.logout((err) => {
     verbose('logout err:', err)
