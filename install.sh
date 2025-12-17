60fe2722bb72c17c9578500c63d0a8a45f1fc4e0
Artem Arakcheev <artarakcheev@gmail.com>
2025-12-18 00:03:45 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+curl https://raw.githubusercontent.com/vuics/h9y/refs/heads/main/docker-compose.yml --output docker-compose.yml && \
+  curl https://raw.githubusercontent.com/vuics/h9y/refs/heads/main/env.example --output .env && \
+  curl https://raw.githubusercontent.com/vuics/h9y/refs/heads/main/gen-certs.sh --output gen-certs.sh && \
+  curl https://raw.githubusercontent.com/vuics/h9y/refs/heads/main/setup-hosts.sh --output setup-hosts.sh && \
+  mkdir -p config/prometheus/ config/traefik/ config/vault/ && \
+  curl https://raw.githubusercontent.com/vuics/h9y/refs/heads/main/config/prometheus/prometheus.yml --output config/prometheus/prometheus.yml && \
+  curl https://raw.githubusercontent.com/vuics/h9y/refs/heads/main/config/traefik/tls.yaml --output config/traefik/tls.yaml && \
+  curl https://raw.githubusercontent.com/vuics/h9y/refs/heads/main/config/vault/config.hcl --output config/vault/config.hcl && \
+  touch .env.api .env.agency && \
+  chmod +x gen-certs.sh setup-hosts.sh && \
+  ./gen-certs.sh
